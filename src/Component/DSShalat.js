19bc4aecd2447d823e1ebd3a03a40ccaf1f26174
Rizqan <calonapoteker@gmail.com>
2019-09-13 14:07:30 +0700

    DATAJson

--- src/Component/DSShalat.js
+++ src/Component/DSShalat.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, ScrollView } from 'react-native'
 import  Data from '../Data Base/DSShalat.json'
+import Hadist from '../Data Base/HadistDSS.json'
 
 export default class DSShalat extends React.Component {
   state = {
-    Data
+    Data,
+    Hadist
   }
 
   //untuk navigasi si pembuat kasih nama DSShalat
@@ -14,6 +16,28 @@ export default class DSShalat extends React.Component {
   render () {
     return (
       <View>
+      <ScrollView>
+      <FlatList
+          data={this.state.Hadist}
+          keyExtractor={(index, item) => index.toString()}
+          renderItem={({ index, item }) => {
+            return (
+
+              // Data.json berisi:
+              //   id, diskripsi, arab, artinya,
+              // kalo ada yg bingung bisa nanya ke si pembuat
+
+              <View>
+
+                <Text>{item.deskripsi}</Text>
+                <Text>{item.arab}</Text>
+                <Text>{item.artinya}</Text>
+                
+              </View>
+            )
+          }}
+        />
+        <Text>Berikut ini adalah bacaan-bacaan dzikir yang shahih setelah shalat fardhu, yang sesuai dengan sunnah Rasulullah Shallallaahu ‘alaihi wasallam. (dibaca setelah salam).</Text>
         <FlatList
           data={this.state.Data}
           keyExtractor={(index, item) => index.toString()}
@@ -25,12 +49,16 @@ export default class DSShalat extends React.Component {
               // kalo ada yg bingung bisa nanya ke si pembuat
 
               <View>
+                <Text>{item.id}</Text>
                 <Text>{item.arab}</Text>
                 <Text>{item.artinya}</Text>
+                <Text>{item.deskripsi}</Text>
               </View>
             )
           }}
         />
+        <Text>Demikian bacaan dzikir yang shahih setelah shalat fardhu sesuai dengan sunnah Rasulullah Shallallaahu ‘alaihi wasallam. Mudah-mudahan dapat memberi kebaikan dan manfaat bagi kita semua.</Text>
+        </ScrollView>
       </View>
     )
   }
